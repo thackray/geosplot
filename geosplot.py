22a766d81dea0209c79432b6fa4c52eece51aa51
thackray <thackray@mit.edu>
2015-10-23 14:37:59 -0400

    added options and docs

--- geosplot.py
+++ geosplot.py
@@ -1,15 +1,54 @@
-import pylab as pl
-import matplotlib.pyplot as plt
-import numpy as np
-from mpl_toolkits.basemap import Basemap
-from mpl_toolkits.axes_grid1 import make_axes_locatable as mal
+try:
+    import pylab as pl
+except ImportError:
+    print "pylab module required but not found"
+
+try:
+    import numpy as np
+except ImportError:
+    print "numpy module required but not found"
+
+try:
+    from mpl_toolkits.basemap import Basemap
+except ImportError:
+    print "basemap module required but not found"
+
+try:
+    from mpl_toolkits.axes_grid1 import make_axes_locatable as mal
+except ImportError:
+    print "axes_grid1 module required but not found"
 
 
 def geosmap(lon, lat, data, proj = 'mill',
             colorticks=None, colormap='seismic',
             figsize=(18,14),
             latlines=None, lonlines=None,
-            ortho0=None):
+            ortho0=None,
+            coastlines=True,
+            coastcolor='white',
+            plottype='pcolor'):
+    """Plot data on map with lon-lat grid.
+    Arguments:
+    lon - array-like longitude points
+    lat - array-like latitude points
+    data - 2D array of points to plot on lon-lat grid
+
+    Keyword Arguments:
+    proj - type of projection to plot map on
+    colormap - color scheme of color axis
+    colorticks - points to label on color axis
+    latlines - latitudes at which to draw lines
+    lonlines - longitudes at which to draw lines
+    figsize - size of figure frame
+    ortho0 - (lon,lat) point to center ortho projection on
+    coastlines - whether to draw coastlines
+    coastcolor - color to make drawn coastlines
+    """
+
+    assert len(lon) in data.shape, 'lon mismatch with data to plot dimension'
+    assert len(lat) in data.shape, 'lat mismatch with data to plot dimension'
+
+    # Set up projection parameters
     if proj == 'mill':
         bmargs = {'projection' : 'mill', 
                   'llcrnrlon' : min(lon), 
@@ -19,6 +58,8 @@ def geosmap(lon, lat, data, proj = 'mill',
                   }
     elif proj == 'ortho':
         if ortho0:
+            assert type(ortho0) in [list, tuple, np.array], 'wrong type for ortho0'
+            assert len(ortho0) == 2, 'ortho0 must be in the form [lon0, lat0]'
             lon0 = ortho0[0]
             lat0 = ortho0[1]
         else:
@@ -31,29 +72,40 @@ def geosmap(lon, lat, data, proj = 'mill',
     else:
         print "Projection not recognized"
         return
+    ####################
 
+    # General figure details
     bm = Basemap(**bmargs)
     lons, lats = np.meshgrid(lon, lat)
     x, y = bm(lons, lats)
 
     pl.figure(figsize=figsize)
     ax = pl.gca()
-    bm.drawcoastlines(linewidth=1.25, color='white')
+
+    if coastlines:
+        bm.drawcoastlines(linewidth=1.25, color=coastcolor)
 
     if latlines:
         parallels = latlines
     else:
         parallels = np.array([-60,-30,0,30,60])
-    bm.drawparallels(parallels, labels=[1,0,0,0])
+    bm.drawparallels(parallels, labels=[1,0,0,0], fontsize=20)
 
     if lonlines:
         meridians = lonlines
     else:
         meridians = np.arange(min(lon),max(lon),60)
-    bm.drawmeridians(meridians, labels=[0,0,0,1])
+    bm.drawmeridians(meridians, labels=[0,0,0,1], fontsize=20)
+    ####################
 
-    bm.pcolor(x,y,data, cmap=colormap)
-
+    # Plotting and colorbar
+    if plottype == 'pcolor':
+        bm.pcolor(x,y,data, cmap=colormap)
+    elif plottype == 'contourf':
+        bm.contourf(x,y,data)
+    else:
+        print "plottype not recognized"
+        raise TypeError
     if colorticks:
         tiks = colorticks
     else:
@@ -64,24 +116,24 @@ def geosmap(lon, lat, data, proj = 'mill',
     cbar = pl.colorbar(cax=cax,ticks=tiks)
 
     tiklabs = ['%.2f'%ti for ti in tiks]
-    cbar.ax.set_yticklabels(tiklabs)
+    cbar.ax.set_yticklabels(tiklabs, fontsize=20)
+    ####################
 
-#    pl.savefig('figs/%s_new_site_metric_%s.png'%(pah,titles[pah][str(tail)]), 
-#               bbox_inches='tight')
-
-def make_z(x,y):
-    xi = len(x)/3
-    yi = 2*len(y)/3
-    xx = np.exp(-((x-xi)/100.)**2)
-    yy = np.exp(-((y-yi)/55.)**2)
-    xx = np.array([list(xx)]*len(y)).T
-    yy = np.array([list(yy)]*len(x))
-    z = np.ones((len(x), len(y)))
-    z = z*xx
-    z = z*yy
-    return z
 
 if __name__=='__main__':
+
+    def make_z(x,y):
+        xi = len(x)/3
+        yi = 2*len(y)/3
+        xx = np.exp(-((x-xi)/100.)**2)
+        yy = np.exp(-((y-yi)/55.)**2)
+        xx = np.array([list(xx)]*len(y)).T
+        yy = np.array([list(yy)]*len(x))
+        z = np.ones((len(x), len(y)))
+        z = z*xx
+        z = z*yy
+        return z
+
     
     noise = np.random.rand(46,72)
     
@@ -94,6 +146,7 @@ if __name__=='__main__':
 
     geosmap(lon,lat,zdata, colorticks = [-5.,-2.5, 0., 2.5, 5])
 
-#    geosmap(lon,lat,zdata, proj='ortho', ortho0 = (50,45), colormap='winter')
+    geosmap(lon,lat,zdata, proj='ortho', ortho0 = (50,45), colormap='winter',
+            plottype='contourf')
     
     pl.show()
