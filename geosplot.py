345c68b0bf17441814afc5edf2a9c09fb40ece11
thackray <thackray@mit.edu>
2015-10-22 23:24:04 -0400

    adding kwargs

---
+++ geosplot.py
@@ -0,0 +1,83 @@
+import pylab as pl
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable as mal
+
+
+def geosmap(lon, lat, data, proj = 'mill',
+            colorticks=None, figsize=(18,14),
+            latlines=None, lonlines=None):
+    if proj == 'mill':
+        bmargs = {'projection' : 'mill', 
+                  'llcrnrlon' : min(lon), 
+                  'llcrnrlat' : min(lat),
+                  'urcrnrlon' : max(lon), 
+                  'urcrnrlat' : max(lat)
+                  }
+    else:
+        print "Projection not recognized"
+        return
+
+    bm = Basemap(**bmargs)
+    lons, lats = np.meshgrid(lon, lat)
+    x, y = bm(lons, lats)
+
+    pl.figure(figsize=figsize)
+    ax = pl.gca()
+    bm.drawcoastlines(linewidth=1.25, color='white')
+
+    if latlines:
+        parallels = latlines
+    else:
+        parallels = np.array([-60,-30,0,30,60])
+    bm.drawparallels(parallels, labels=[1,0,0,0])
+
+    if lonlines:
+        meridians = lonlines
+    else:
+        meridians = np.arange(min(lon),max(lon),60)
+    bm.drawmeridians(meridians, labels=[0,0,0,1])
+
+    bm.pcolor(x,y,data, cmap='winter')
+
+    if colorticks:
+        tiks = colorticks
+    else:
+        tiks = np.linspace(np.min(data),np.max(data),10)
+
+    divider = mal(ax)
+    cax = divider.append_axes("right", size='5%', pad=0.05)
+    cbar = pl.colorbar(cax=cax,ticks=tiks)
+
+    tiklabs = ['%.2f'%ti for ti in tiks]
+    cbar.ax.set_yticklabels(tiklabs)
+
+#    pl.savefig('figs/%s_new_site_metric_%s.png'%(pah,titles[pah][str(tail)]), 
+#               bbox_inches='tight')
+
+def make_z(x,y):
+    xi = len(x)/3
+    yi = 2*len(y)/3
+    xx = np.exp(-((x-xi)/100.)**2)
+    yy = np.exp(-((y-yi)/55.)**2)
+    xx = np.array([list(xx)]*len(y)).T
+    yy = np.array([list(yy)]*len(x))
+    z = np.ones((len(x), len(y)))
+    z = z*xx
+    z = z*yy
+    return z
+
+if __name__=='__main__':
+    
+    noise = np.random.rand(46,72)
+    
+    lon = np.linspace(-180, 175, 72)
+    lat = np.linspace(-90, 90, 46)
+    lat[0] = -89.
+    lat[-1] = 89.
+
+    zdata = 10*make_z(lon,lat).T + noise
+
+    geosmap(lon,lat,zdata)
+    pl.show()
