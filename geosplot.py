c58d1c3c92ac359c0f1fda4a15102fceb30fb803
thackray <thackray@mit.edu>
2015-12-01 15:23:45 -0500

    fixed lonlines truth bug

--- geosplot.py
+++ geosplot.py
@@ -135,7 +135,7 @@ def geosmap(lon, lat, data, proj = 'mill',
     if not proj in ['ortho']:
         bm.drawparallels(parallels, labels=[1,0,0,0], fontsize=20)
 
-    if lonlines:
+    if lonlines not None:
         meridians = lonlines
     else:
         meridians = np.arange(min(lon),max(lon),60)
